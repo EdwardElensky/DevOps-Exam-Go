925f73e692999d3fca1bef45e9a066eacb73be02
Ed Elensky <elenskyed516@gmail.com>
2021-04-12 12:02:12 +0300

    edit code for test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+        fmt.Fprintf(w, "Hello, World. you've requested: %s\n", r.URL.Path)
     })
 
     http.ListenAndServe(":3000", nil)
